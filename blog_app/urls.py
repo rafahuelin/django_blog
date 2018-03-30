7a9639b581515873ff8d176045c309e26e5c7520
Rafa <rafahuelin@gmail.com>
2018-03-30 09:42:41 +0200

    build list and detail views

---
+++ blog_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog_app'
+
+urlpatterns = [
+    # post views
+    path('', views.post_list, name='post_list'),
+    # post details
+    path('<int:year>/<int:month>/<int:day>/<post>',
+         views.post_detail,
+         name='post_detail'),
+]

82b9efd9a1ff131fc53b5e08e0209d4f97a5eb26
Rafa <rafahuelin@gmail.com>
2018-04-01 10:54:57 +0200

    retrieve posts by similarity

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
@@ -65,9 +66,15 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog_app/post/detail.html', {'post': post,
                                                          'comments': comments,
-                                                         'comment_form': comment_form})
+                                                         'comment_form': comment_form,
+                                                         'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
